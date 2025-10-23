e0f94fc21e208d8448a878bb157ae35a7858a8a7
zry <164117208@qq.com>
2025-10-23 20:20:23 +0800

    API Math

---
+++ api_Math/Test.java
@@ -0,0 +1,18 @@
+package api_Math;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-3));
+        System.out.println(Math.abs(-2.34));
+
+        System.out.println(Math.ceil(4.5));
+        System.out.println(Math.floor(4.01));
+
+        System.out.println(Math.round(5.4));
+        System.out.println(Math.max(4, 5));
+
+        System.out.println(Math.pow(2, 4));
+
+        System.out.println(Math.random());
+    }
+}
